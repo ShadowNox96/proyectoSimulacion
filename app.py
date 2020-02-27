df21661c26f392b4cbd98d00575fbf3d3695a089
ShadowNox96 <cdcarlose32@gmail.com>
2020-02-26 19:18:08 -0600

    Crud de productos terminado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -19,16 +19,61 @@ def indexPage():
 @app.route('/products')
 def listProducts():
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM costo')
+    cursor.execute('SELECT * FROM producto')
     data = cursor.fetchall()
     return render_template('productos.html', data = data)
 
 
 
+@app.route('/addProduct')
+def nProduct():
+    return render_template('nProducto.html')
+
+@app.route('/nProduct', methods =['POST'])
+def addProduct():
+    if request.method == 'POST':
+        nameProduct = request.form['name']
+        price = float(request.form['price']) 
+    
+    cursor = mysql.get_db().cursor()
+    cursor.execute('INSERT INTO producto (nomProducto,precio) VALUES(%s, %s)', (nameProduct, price))
+    cursor.connection.commit()
+    return redirect(url_for('listProducts'))
 
 
+@app.route('/editProduct/<id>')
+def editProduct(id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM producto where idProducto = %s', [id])
+    data = cursor.fetchall()
+    print(data)
+    return render_template('editProduct.html', data= data[0])
 
+@app.route('/saveEditProduct/<id>', methods=['POST'])
+def saveProduct(id):
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+    
+    cursor = mysql.get_db().cursor()
+    cursor.execute('UPDATE producto SET nomProducto = %s, precio=%s where idProducto = %s',(name,price,id))
+    cursor.connection.commit()
+    return redirect(url_for('listProducts'))
 
+@app.route('/deleteProduct/<id>')
+def deleteProduct(id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('delete from producto where idProducto= %s',(id))
+    cursor.connection.commit()
+    return redirect(url_for('listProducts'))
+
+@app.route('/simulation')
+def simulation():
+    return render_template('simulacion.html')
+
+@app.route('/cost')
+def listCosts():
+    return render_template('costs.html')
 
 
 
